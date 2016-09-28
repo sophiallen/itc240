22338fe24cbc862092db86c919e2fa863a37c19e
Sophia Allen <smlelevich@gmail.com>
2016-09-28 16:13:28 -0700

    created first.php

---
+++ first.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Hello, world!</title>
+	</head>
+	<body>
+		<p>
+			Hello, world!
+		</p>
+		<h4>
+			<?php
+				//this is a comment
+        #this is also a comment
+        
+				/*This is a 
+        multiple-line
+        comment*/
+        
+        //echo writes to the window
+				echo 'This is php'; 
+			?>			
+		</h4>	
+	</body>
+</html>
